8ba36b89ac1084ee5ab25bed8056cd5558839aa6
aafaq-hassan <aafaqhassan@gmail.com>
2018-05-16 20:24:44 +0500

    Fix value call on Rails 5.2 (#47)

--- lib/currency_select.rb
+++ lib/currency_select.rb
@@ -98,7 +98,11 @@ module ActionView
       def to_currency_select_tag(priority_currencies, options, html_options)
         html_options = html_options.stringify_keys
         add_default_name_and_id(html_options)
-        value = value(object)
+        value = if self.method(:value).arity == 0
+                  value()
+                else
+                  value(object)
+                end
         content_tag('select', add_options(currency_options_for_select(value, priority_currencies), options, value), html_options)
       end
     end
